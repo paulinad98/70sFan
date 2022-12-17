37a28081be220f30d12fe6651b88bc6013e83b90
Paulina <paulinaklempka@gmail.com>
2022-12-17 10:54:08 +0100

    Landing: add template

--- frontend/landing/pages/index.vue
+++ frontend/landing/pages/index.vue
@@ -1,7 +1,33 @@
-<template></template>
+<template>
+  <div>
+    <app-navigation />
+    <the-header />
+    <section-one />
+    <section-two />
+    <section-three />
+    <join-us />
+    <app-footer />
+  </div>
+</template>
 
 <script>
-export default {};
-</script>
+import AppFooter from "@/components/app/AppFooter.vue";
+import AppNavigation from "@/components/app/AppNavigation.vue";
+import TheHeader from "@/components/landing/TheHeader.vue";
+import SectionOne from "@/components/landing/SectionOne.vue";
+import SectionTwo from "@/components/landing/SectionTwo.vue";
+import SectionThree from "@/components/landing/SectionThree.vue";
+import JoinUs from "@/components/landing/JoinUs.vue";
 
-<style scoped></style>
+export default {
+  components: {
+    AppFooter,
+    AppNavigation,
+    TheHeader,
+    SectionOne,
+    SectionTwo,
+    SectionThree,
+    JoinUs,
+  },
+};
+</script>
