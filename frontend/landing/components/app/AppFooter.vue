da0aad02ce69a2f187949ff187943ffc8831b940
Paulina <paulinaklempka@gmail.com>
2022-12-17 19:56:21 +0100

    Set join-us section
    modified:   frontend/landing/components/landing/JoinUs.vue
    Landing: set join-us section

--- frontend/landing/components/app/AppFooter.vue
+++ frontend/landing/components/app/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="bg-white dark:bg-gray-800">
-    <div class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
+    <div class="max-w-screen-xl p-4 py-6 mx-auto md:p-8 lg:p-10">
       <hr
         class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"
       />
