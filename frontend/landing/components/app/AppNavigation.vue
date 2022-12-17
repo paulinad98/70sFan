97e458a27e53a7b7350d1442a5010585cc775c04
Paulina <paulinaklempka@gmail.com>
2022-12-17 19:06:50 +0100

    Landing: set navigation

--- frontend/landing/components/app/AppNavigation.vue
+++ frontend/landing/components/app/AppNavigation.vue
@@ -1,37 +1,17 @@
 <template>
-  <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 fixed w-full">
+  <nav class="bg-white border-gray-200 py-6 dark:bg-gray-900 fixed w-full">
     <div
       class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto"
     >
-      <a href="#" class="flex items-center">
-        <img
-          src="./images/logo.svg"
-          class="h-6 mr-3 sm:h-9"
-          alt="Landwind Logo"
-        />
-        <span
-          class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-          >Landwind</span
-        >
-      </a>
+      <app-logo class="flex items-center" />
+
       <div class="flex items-center lg:order-2">
-        <div class="hidden mt-2 mr-4 sm:inline-block">
-          <a
-            class="github-button"
-            href="https://github.com/themesberg/landwind"
-            data-size="large"
-            data-icon="octicon-star"
-            data-show-count="true"
-            aria-label="Star themesberg/landwind on GitHub"
-            >Star</a
-          >
-        </div>
-        <!-- <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a> -->
         <a
           href="https://themesberg.com/product/tailwind-css/landing-page"
           class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
-          >Download</a
         >
+          Sign with Pareon
+        </a>
         <button
           data-collapse-toggle="mobile-menu-2"
           type="button"
@@ -73,7 +53,7 @@
         <ul
           class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0"
         >
-          <li>
+          <!-- <li>
             <a
               href="#"
               class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white"
@@ -87,37 +67,19 @@
               class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
               >Company</a
             >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-              >Marketplace</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-              >Features</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-              >Team</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-              >Contact</a
-            >
-          </li>
+          </li> -->
         </ul>
       </div>
     </div>
   </nav>
 </template>
+
+<script>
+import AppLogo from "@/components/app/AppLogo.vue";
+
+export default {
+  components: {
+    AppLogo,
+  },
+};
+</script>
