63a91f7c389f716d6d257ca4afc9b2c4ef9a991b
Paulina <paulinaklempka@gmail.com>
2022-12-17 11:08:38 +0100

    Landing: set header

--- frontend/landing/components/landing/TheHeader.vue
+++ frontend/landing/components/landing/TheHeader.vue
@@ -7,90 +7,65 @@
         <h1
           class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white"
         >
-          Building digital <br />products & brands.
+          Basketball history
         </h1>
         <p
           class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
         >
-          This free and open-source landing page template was built using the
-          utility classes from
-          <a href="https://tailwindcss.com" class="hover:underline"
-            >Tailwind CSS</a
-          >
-          and based on the components from the
-          <a
-            href="https://flowbite.com/docs/getting-started/introduction/"
-            class="hover:underline"
-            >Flowbite Library</a
-          >
-          and the
-          <a href="https://flowbite.com/blocks/" class="hover:underline"
-            >Blocks System</a
-          >.
+          I am running the channel on YouTube about basketball history. My main
+          work is about collecting rare, unseen NBA footage from late 1950s to
+          late 1980s. I am also creating long video breakdowns of some forgotten
+          NBA legends from that period. If you are also basketball fan, help me
+          purchasing footage from archives and private collectors.
         </p>
         <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
           <a
-            href="https://github.com/themesberg/landwind"
+            href="https://www.youtube.com/channel/UCQ-frNSTRgnLPgm9JWaL6Vw"
             class="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           >
             <svg
-              class="w-4 h-4 mr-2 text-gray-500 dark:text-gray-400"
+              class="w-4 h-4 mr-2"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 496 512"
+              height="800"
+              width="1200"
+              viewBox="-35.20005 -41.33325 305.0671 247.9995"
             >
-              <!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) -->
               <path
-                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+                d="M229.763 25.817c-2.699-10.162-10.65-18.165-20.748-20.881C190.716 0 117.333 0 117.333 0S43.951 0 25.651 4.936C15.553 7.652 7.6 15.655 4.903 25.817 0 44.236 0 82.667 0 82.667s0 38.429 4.903 56.85C7.6 149.68 15.553 157.681 25.65 160.4c18.3 4.934 91.682 4.934 91.682 4.934s73.383 0 91.682-4.934c10.098-2.718 18.049-10.72 20.748-20.882 4.904-18.421 4.904-56.85 4.904-56.85s0-38.431-4.904-56.85"
+                fill="red"
+              />
+              <path
+                d="M93.333 117.559l61.333-34.89-61.333-34.894z"
+                fill="#fff"
               />
             </svg>
-            View on GitHub
+
+            View on YouTube
           </a>
           <a
-            href="https://www.figma.com/community/file/1125744163617429490"
+            href="https://www.patreon.com/70sFan"
             class="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           >
             <svg
               class="w-4 h-4 mr-2"
-              id="Layer_1"
+              width="1024px"
+              height="1024px"
+              viewBox="0 0 1024 1024"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 200 300"
-              width="1667"
-              height="2500"
             >
-              <title>Figma.logo</title>
-              <desc>Created using Figma</desc>
+              <circle cx="512" cy="512" r="512" style="fill: #ff424d" />
               <path
-                id="path0_fill"
-                class="st0"
-                d="M50 300c27.6 0 50-22.4 50-50v-50H50c-27.6 0-50 22.4-50 50s22.4 50 50 50z"
-              />
-              <path
-                id="path1_fill"
-                class="st1"
-                d="M0 150c0-27.6 22.4-50 50-50h50v100H50c-27.6 0-50-22.4-50-50z"
-              />
-              <path
-                id="path1_fill_1_"
-                class="st2"
-                d="M0 50C0 22.4 22.4 0 50 0h50v100H50C22.4 100 0 77.6 0 50z"
-              />
-              <path
-                id="path2_fill"
-                class="st3"
-                d="M100 0h50c27.6 0 50 22.4 50 50s-22.4 50-50 50h-50V0z"
-              />
-              <path
-                id="path3_fill"
-                class="st4"
-                d="M200 150c0 27.6-22.4 50-50 50s-50-22.4-50-50 22.4-50 50-50 50 22.4 50 50z"
+                d="M598.57 285.68c-93.7 0-169.94 76.24-169.94 169.94C428.63 549 504.87 625 598.57 625 692 625 768 549 768 455.62c0-93.7-76-169.94-169.43-169.94M296 738.32h83V285.68h-83"
+                style="fill: #fff"
               />
             </svg>
-            Get Figma file
+
+            View on Patreon
           </a>
         </div>
       </div>
-      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-        <img src="./images/hero.png" alt="hero image" />
+      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex lg:justify-end">
+        <img src="~/assets/img/header-img.jpg" alt="hero image" />
       </div>
     </div>
   </header>
