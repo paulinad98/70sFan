bce31ff26ea0160d176e6e1afc1391ce0f377d27
Paulina <paulinaklempka@gmail.com>
2022-12-17 19:27:50 +0100

    Landing: change styles in header

--- frontend/landing/components/landing/TheHeader.vue
+++ frontend/landing/components/landing/TheHeader.vue
@@ -1,16 +1,16 @@
 <template>
-  <header class="bg-white dark:bg-gray-900">
+  <header class="bg-white dark:bg-gray-900 py-11">
     <div
       class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28"
     >
       <div class="mr-auto place-self-center lg:col-span-7">
         <h1
-          class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white"
+          class="max-w-2xl mb-6 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white"
         >
           Basketball history
         </h1>
         <p
-          class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
+          class="max-w-2xl mb-8 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 leading-relaxed"
         >
           I am running the channel on YouTube about basketball history. My main
           work is about collecting rare, unseen NBA footage from late 1950s to
@@ -65,7 +65,11 @@
         </div>
       </div>
       <div class="hidden lg:mt-0 lg:col-span-5 lg:flex lg:justify-end">
-        <img src="~/assets/img/header-img.jpg" alt="hero image" />
+        <img
+          class="rounded-xl"
+          src="~/assets/img/header-img.jpg"
+          alt="hero image"
+        />
       </div>
     </div>
   </header>
