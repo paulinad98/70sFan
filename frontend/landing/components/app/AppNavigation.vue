1c76070fb1fd771ecbca67dbff8161d897050636
Paulina <paulinaklempka@gmail.com>
2022-12-19 20:24:27 +0100

    Landing: change focus in btns

--- frontend/landing/components/app/AppNavigation.vue
+++ frontend/landing/components/app/AppNavigation.vue
@@ -8,7 +8,7 @@
       <div class="flex items-center lg:order-2">
         <a
           :href="url"
-          class="text-white bg-purple-700 hover:bg-gray-500 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none"
+          class="text-white bg-purple-700 hover:bg-gray-500 focus:ring-4 focus:ring-white font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none"
         >
           Sign with Pareon
         </a>
