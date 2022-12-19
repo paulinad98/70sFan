7e008ac0b48e4d53651119f59daa9c08a9ee41dd
Paulina <paulinaklempka@gmail.com>
2022-12-19 22:01:51 +0100

    API: connect with patreon, get user token

--- api/controllers/oauth.js
+++ api/controllers/oauth.js
@@ -1,3 +1,21 @@
-exports.oauthClient = (req, res, next) => {
-  console.log("hej");
+const { oauth } = require("patreon");
+
+const clientId = process.env.PATREON_CLIENT_ID;
+const clientSecret = process.env.PATREON_CLIENT_SECRET;
+const redirect = process.env.PATREON_REDIRECT_URI;
+
+const oauthClientPatreon = oauth(clientId, clientSecret);
+
+exports.oauthClient = async (req, res, next) => {
+  const { code } = req.query;
+
+  return oauthClientPatreon
+    .getTokens(code, redirect)
+    .then(({ access_token }) => {
+      token = access_token;
+      console.log(token);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
