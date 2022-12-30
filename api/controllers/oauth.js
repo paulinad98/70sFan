cbacbd504864daaaa4478982bd70f5d2b5ab6037
Paulina <paulinaklempka@gmail.com>
2022-12-30 20:49:25 +0100

    API: add fetch patreon data from patreon API

--- api/controllers/oauth.js
+++ api/controllers/oauth.js
@@ -1,4 +1,6 @@
 const { oauth } = require("patreon");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const clientId = process.env.PATREON_CLIENT_ID;
 const clientSecret = process.env.PATREON_CLIENT_SECRET;
@@ -6,12 +8,32 @@ const redirect = process.env.PATREON_REDIRECT_URI;
 
 const oauthClientPatreon = oauth(clientId, clientSecret);
 
+const url =
+  "https://www.patreon.com/api/oauth2/v2/identity?include=memberships&fields%5Bmember%5D=last_charge_status,full_name";
+
+const options = (token) => {
+  return {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+};
+
 exports.oauthClient = async (req, res, next) => {
   const { code } = req.query;
 
   try {
     const { access_token } = await oauthClientPatreon.getTokens(code, redirect);
-    console.log(access_token);
+
+    const response = await fetch(url, options(access_token));
+    const patreonData = await response.json();
+    const patreonDataIncluded = patreonData.included[0];
+
+    const { id } = patreonDataIncluded;
+    const { full_name, last_charge_status } = patreonDataIncluded.attributes;
+
+    console.log(id, full_name, last_charge_status);
   } catch (err) {
     console.log(err);
   }
