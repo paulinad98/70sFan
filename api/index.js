ff31d5de478b22c0e76ea1044c5fabee71138918
Paulina <paulinaklempka@gmail.com>
2023-01-02 21:47:07 +0100

    API: setup game and team in index.js

--- api/index.js
+++ api/index.js
@@ -4,15 +4,21 @@ const express = require("express");
 const sequelize = require("./util/database");
 
 const Patreon = require("./models/patreon");
+const Team = require("./models/team");
+const Game = require("./models/game");
 
 const oauthRoutes = require("./routes/oauth");
+const gameRoutes = require("./routes/game");
+const teamRoutes = require("./routes/team");
 
 const app = express();
 app.use("/oauth", oauthRoutes);
+app.use("/game", gameRoutes);
+app.use("/team", teamRoutes);
 
 const startServer = async () => {
   try {
-    await sequelize.sync();
+    await sequelize.sync({ force: true });
     app.listen(5000, () => {
       console.log("Server listening on port 5000");
     });
