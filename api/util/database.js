4b1096f931a8de91aa181edcfeeff6e772176a0d
Paulina <paulinaklempka@gmail.com>
2023-01-06 18:12:00 +0100

    API: configure vercel

--- api/util/database.js
+++ api/util/database.js
@@ -1,4 +1,5 @@
 const Sequelize = require("sequelize");
+const mysql2 = require("mysql2");
 
 const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 const DATABASE_HOST = process.env.DATABASE_HOST;
@@ -9,6 +10,7 @@ const sequelize = new Sequelize("70sfan", DATABASE_USER, DATABASE_PASSWORD, {
   dialect: "mysql",
   host: DATABASE_HOST,
   port: DATABASE_PORT,
+  dialectModule: mysql2,
 });
 
 module.exports = sequelize;
