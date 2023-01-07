dbc4c095c33312f2ea1e7ed8cf6911d3d879fe7d
Paulina <paulinaklempka@gmail.com>
2023-01-07 21:10:14 +0100

    Frontend app: setup vue3

---
+++ frontend/app/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import { RouterView } from "vue-router";
+
+export default {
+  components: {
+    RouterView,
+  },
+};
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped></style>
