eb9fcf5c79ff7e70f39cdc2ba4ee311424ef0a22
Paulina <paulinaklempka@gmail.com>
2023-01-08 21:18:28 +0100

    Frontend app: add base button

---
+++ frontend/app/src/components/base/BaseButton.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <button
+    type="button"
+    class="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+  >
+    <slot name="prevIcon"></slot>
+    <slot></slot>
+    <slot name="nextIcon"></slot>
+  </button>
+</template>
--- frontend/app/src/views/PanelView.vue
+++ frontend/app/src/views/PanelView.vue
@@ -1,12 +1,20 @@
 <script setup>
 import PanelSidebar from "@/components/panel/PanelSidebar.vue";
+import BaseButton from "@/components/base/BaseButton.vue";
+import PlusIcon from "@/components/icons/PlusIcon.vue";
 </script>
 
 <template>
-  <div class="flex gap-14">
+  <div class="flex gap-14 items-start">
     <panel-sidebar />
-    <h1>
-      Panel - <span class="capitalize">{{ $route.params.tab || "Teams" }}</span>
-    </h1>
+    <div class="flex items-center gap-7">
+      <h1 class="text-2xl text-gray-900 font-medium">
+        Panel -
+        <span class="capitalize">{{ $route.params.tab || "Teams" }}</span>
+      </h1>
+      <base-button class="bg-primary-50 hover:bg-gray-700">
+        <template #prevIcon><plus-icon class="w-2 h-2 mr-2" /></template> Add
+      </base-button>
+    </div>
   </div>
 </template>
