ea25c34bdd0cf0a47dec9f7ceabe4fb1261a22f1
Paulina <paulinaklempka@gmail.com>
2023-01-09 21:30:20 +0100

    Frontend app: Create base form

--- frontend/app/src/components/base/BaseButton.vue
+++ frontend/app/src/components/base/BaseButton.vue
@@ -2,7 +2,6 @@
 
 <template>
   <button
-    type="button"
     class="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
   >
     <slot name="prevIcon"></slot>
---
+++ frontend/app/src/components/base/BaseForm.vue
@@ -0,0 +1,10 @@
+<script setup>
+import BaseButton from "./BaseButton.vue";
+</script>
+
+<template>
+  <form @submit.prevent="">
+    <template></template>
+    <base-button type="submit">Submit</base-button>
+  </form>
+</template>
