e3df4546a4b110e00af74ca89d5dbd49d5b85899
Paulina <paulinaklempka@gmail.com>
2023-01-17 19:11:53 +0100

    Fix fetch composables

--- frontend/app/src/composables/useFetchApi.js
+++ frontend/app/src/composables/useFetchApi.js
@@ -1,18 +1,24 @@
 const API_URL = import.meta.env.VITE_API_URL;
+const API_TOKEN = import.meta.env.VITE_API_TOKEN;
 
 export function useFetchApi() {
-  const getData = async ({ method = "GET", payload = {}, endpoint = "" }) => {
+  const useFetch = async ({ method = "GET", payload = {}, endpoint = "" }) => {
+    const body = method !== "GET" && JSON.stringify(payload);
+    const options = {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        "x-auth-token": API_TOKEN,
+      },
+      mode: "cors",
+    };
+
+    if (body) {
+      options.body = body;
+    }
+
     try {
-      const response = await fetch(`${API_URL}/${endpoint}`, {
-        method,
-        headers: {
-          "Content-Type": "application/json",
-          "x-auth-token":
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImYzZDM0Zjk5LTQwYWItNGMzMC1iYjAwLTA4ZGU4ZGJkODMxYiIsImZ1bGxfbmFtZSI6IlBhdWxpbmEgRCIsImlhdCI6MTY3MjY5Mjk5MywiZXhwIjoxNjczODE2MTkzfQ.BBP2EzpfiRkDheum-3oB9HZy4fVg6bymdmHrodCnIPY",
-        },
-        mode: "cors",
-        body: JSON.stringify(payload),
-      });
+      const response = await fetch(`${API_URL}/${endpoint}`, options);
       const data = await response.json();
 
       return { data };
@@ -21,5 +27,5 @@ export function useFetchApi() {
     }
   };
 
-  return { getData };
+  return { useFetch };
 }
