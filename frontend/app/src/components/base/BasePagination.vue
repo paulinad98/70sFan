1440b890787ea5b7b62f060aebf7a9ec778b3b01
Paulina <paulinaklempka@gmail.com>
2023-03-11 00:16:04 +0100

    Frontend app: refactor pagination

--- frontend/app/src/components/base/BasePagination.vue
+++ frontend/app/src/components/base/BasePagination.vue
@@ -1,5 +1,9 @@
 <script setup>
-defineProps({
+import NextIcon from "@/components/icons/NextIcon.vue";
+import PrevIcon from "@/components/icons/PrevIcon.vue";
+
+import { computed, h } from "vue";
+const props = defineProps({
   lastPage: {
     type: Number,
     required: true,
@@ -7,82 +11,88 @@ defineProps({
   modelValue: {
     type: Number,
   },
+  maxPage: {
+    type: Number,
+    default: 5,
+  },
 });
 
-defineEmits(["update:modelValue"]);
+const emits = defineEmits(["update:modelValue", "change"]);
+
+const limitPage = computed(() => {
+  if (props.maxPage > props.lastPage) {
+    return props.lastPage;
+  }
+
+  return props.maxPage;
+});
+
+const offsetPage = computed(() => {
+  if (props.modelValue > props.lastPage - limitPage.value) {
+    return props.lastPage - limitPage.value - 1;
+  }
+
+  return props.modelValue - 1;
+});
+
+const PaginationButton = ({ page }, { slots }) => {
+  const baseClasses =
+    "px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700";
+  const highlightClasses =
+    "z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700";
+
+  return h(
+    "li",
+    h(
+      "button",
+      {
+        onClick: () => {
+          emits("update:modelValue", page);
+          emits("change");
+        },
+        class: [
+          baseClasses,
+          props.modelValue === page && !slots.default && highlightClasses,
+        ],
+      },
+      slots.default ? slots.default() : page
+    )
+  );
+};
 </script>
 
 <template>
   <nav aria-label="pagination">
-    <ul class="inline-flex items-center -space-x-px">
-      <li>
-        <button
-          @click="
-            $emit('update:modelValue', modelValue - 1 > 0 ? modelValue - 1 : 1),
-              $emit('change')
-          "
-          class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
-        >
-          <span class="sr-only">Previous</span>
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
+    <ul class="inline-flex items-center">
+      <pagination-button :page="modelValue - 1 > 0 ? modelValue - 1 : 1">
+        <span class="sr-only">Previous</span>
+        <prev-icon />
+      </pagination-button>
+
+      <pagination-button v-if="modelValue !== 1" :page="1" />
+      <li v-if="modelValue !== 1" class="text-gray-500 mx-1">...</li>
+
+      <pagination-button
+        :key="`page-${page}`"
+        v-for="page in limitPage"
+        :page="page + offsetPage"
+      />
+
+      <li
+        v-if="offsetPage + limitPage + 1 !== lastPage"
+        class="text-gray-500 mx-1"
+      >
+        ...
       </li>
 
-      <li>
-        <button
-          :key="`page-${page}`"
-          v-for="page in lastPage"
-          @click="$emit('update:modelValue', page), $emit('change')"
-          aria-current="page"
-          :class="
-            modelValue === page
-              ? 'z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700'
-              : 'px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700'
-          "
-        >
-          {{ page }}
-        </button>
-      </li>
+      <pagination-button :page="lastPage" />
 
-      <li>
-        <button
-          @click="
-            $emit(
-              'update:modelValue',
-              modelValue + 1 <= lastPage ? modelValue + 1 : lastPage
-            ),
-              $emit('change')
-          "
-          class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"
-        >
-          <span class="sr-only">Next</span>
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
-      </li>
+      <pagination-button
+        :page="modelValue + 1 <= lastPage ? modelValue + 1 : lastPage"
+      >
+        <span class="sr-only">Next</span>
+        <next-icon />
+      </pagination-button>
     </ul>
   </nav>
 </template>
