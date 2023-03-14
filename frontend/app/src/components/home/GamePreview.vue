0179fecf310c353b1dd1c60d6c00a0ca078cf6b2
Paulina <paulinaklempka@gmail.com>
2023-03-14 20:53:52 +0100

    Frontend app: refactor GamePreview component

--- frontend/app/src/components/home/GamePreview.vue
+++ frontend/app/src/components/home/GamePreview.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { defineProps } from "vue";
+import { h } from "vue";
+import GamePreviewInfo from "@/components/home/GamePreviewInfo.vue";
 
 const props = defineProps({ game: Object });
 
@@ -9,72 +10,69 @@ const homeTeam = props.game.teams.find(
 const awayTeam = props.game.teams.find(
   (team) => team.id === props.game.awayTeamId
 );
+
+const TeamLogo = (props) => {
+  return h(
+    "div",
+    {
+      class: "text-center mb-2",
+    },
+    [
+      h("img", {
+        class: "w-20 h-20 object-contain",
+        src: props.src,
+        alt: `${props.name} logo`,
+      }),
+      h("span", { class: "text-xs text-gray-800 text-center" }, props.name),
+    ]
+  );
+};
+
+const TeamScore = (props) => {
+  return h(
+    "span",
+    {
+      class: [
+        "text-4xl font-bold",
+        props.score > props.opponentScore ? "text-green-700" : "text-gray-800",
+      ],
+    },
+    props.score
+  );
+};
 </script>
 
 <template>
-  <div class="p-5 border-2 border-gray-700 rounded-md shadow-lg">
+  <div class="p-5 border-2 border-gray-700 bg-white rounded-md shadow-lg">
     <div
       class="grid grid-cols-[112px_auto_112px] grid-rows-[repeat(2,_auto)] justify-items-center items-center mb-5 mx-auto"
     >
-      <img
-        class="w-28 h-28 object-contain col-start-1"
-        :src="homeTeam.logoUrl"
-        :alt="`${homeTeam.name} logo`"
+      <team-logo :src="homeTeam.logoUrl" :name="homeTeam.name" />
+      <team-score
+        :score="game.homeTeamScore"
+        :opponent-score="game.awayTeamScore"
+        class="row-start-2"
       />
-      <span
-        class="row-start-2 text-4xl font-bold"
-        :class="
-          game.homeTeamScore > game.awayTeamScore
-            ? 'text-green-700'
-            : 'text-gray-800'
-        "
-      >
-        {{ game.homeTeamScore }}
-      </span>
+
       <div class="row-start-1 row-end-3 text-center">
-        <span
-          class="text-xl font-medium tracking-wider text-gray-900 block mb-1"
-        >
-          {{ game.season }}
-        </span>
-        <time
-          class="block text-xs font-light tracking-wide mb-3 uppercase"
-          datetime="1998-10-12"
-        >
-          {{
-            new Date(game.date).toLocaleDateString("en", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })
-          }}
-        </time>
-        <a
-          class="text-base font-medium text-blue-600 hover:underline"
-          :href="game.basketballReferenceUrl"
-        >
-          Basketball Reference
-        </a>
+        <game-preview-info
+          :season="game.season"
+          :date="game.date"
+          :basketball-reference-url="game.basketballReferenceUrl"
+        />
       </div>
-      <img
-        class="w-28 h-28 object-contain"
-        :src="awayTeam.logoUrl"
-        :alt="`${awayTeam.name} logo`"
+
+      <team-logo :src="awayTeam.logoUrl" :name="awayTeam.name" />
+      <team-score
+        :score="game.homeTeamScore"
+        :opponent-score="game.awayTeamScore"
       />
-      <span
-        class="text-4xl font-bold"
-        :class="
-          game.awayTeamScore > game.homeTeamScore
-            ? 'text-green-700'
-            : 'text-gray-800'
-        "
-      >
-        {{ game.awayTeamScore }}
-      </span>
     </div>
+
     <p class="mb-4 text-sm leading-normal">
       {{ game.description }}
     </p>
+
     <div class="relative">
       <iframe
         class="w-full h-56"
---
+++ frontend/app/src/components/home/GamePreviewInfo.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+  season: String,
+  date: String,
+  basketballReferenceUrl: String,
+});
+</script>
+
+<template>
+  <span class="text-xl font-medium tracking-wider text-gray-900 block mb-1">
+    {{ season }}
+  </span>
+  <time
+    class="block text-xs font-light tracking-wide mb-3 uppercase"
+    datetime="1998-10-12"
+  >
+    {{
+      new Date(date).toLocaleDateString("en", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      })
+    }}
+  </time>
+  <a
+    class="text-base font-medium text-blue-600 hover:underline"
+    :href="basketballReferenceUrl"
+  >
+    Basketball Reference
+  </a>
+</template>
