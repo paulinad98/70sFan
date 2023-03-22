c4df8eebb48fdc813b59ec3271f3bb0aaca51b58
Paulina <paulinaklempka@gmail.com>
2023-03-22 19:11:00 +0100

    API: add new env variable

--- api/util/database.js
+++ api/util/database.js
@@ -5,12 +5,18 @@ const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 const DATABASE_HOST = process.env.DATABASE_HOST;
 const DATABASE_PORT = process.env.DATABASE_PORT;
 const DATABASE_USER = process.env.DATABASE_USER;
+const DATABASE_NAME = process.env.DATABASE_NAME;
 
-const sequelize = new Sequelize("70sfan", DATABASE_USER, DATABASE_PASSWORD, {
-  dialect: "mysql",
-  host: DATABASE_HOST,
-  port: DATABASE_PORT,
-  dialectModule: mysql2,
-});
+const sequelize = new Sequelize(
+  DATABASE_NAME,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  {
+    dialect: "mysql",
+    host: DATABASE_HOST,
+    port: DATABASE_PORT,
+    dialectModule: mysql2,
+  }
+);
 
 module.exports = sequelize;
