a58361aed2976a2572dff7b361790538e9892268
Paulina <paulinaklempka@gmail.com>
2023-05-18 23:40:17 +0200

    Frontend app: delete console.log

--- frontend/landing/stores/user.js
+++ frontend/landing/stores/user.js
@@ -14,7 +14,6 @@ export const useUserStore = defineStore("user", () => {
 
   function setUserToken(token) {
     user.value.token = token;
-    console.log(user.value.token);
   }
 
   function getToken() {
