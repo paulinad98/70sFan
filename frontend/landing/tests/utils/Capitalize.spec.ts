474742c7489d7059210bf25e056778e88e31aa89
Paulina <paulinaklempka@gmail.com>
2023-05-28 15:32:34 +0200

    Frontend app: add typescript refactor to capitalize function

---
+++ frontend/landing/tests/utils/Capitalize.spec.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from "vitest";
+
+import capitalize from "@/utils/capitalize";
+
+describe("capitalize function", () => {
+    it('should capitalize the first letter of a word', () => {
+    expect(capitalize('hello')).toBe('Hello');
+    expect(capitalize('world')).toBe('World');
+    expect(capitalize('typescript')).toBe('Typescript');
+  });
+
+  it('should return an empty string if no word is provided', () => {
+    expect(capitalize('')).toBe('');
+  });
+});
