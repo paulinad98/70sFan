f6d653647bb005f21002472cd68868c36cd945e0
Paulina <paulinaklempka@gmail.com>
2023-05-30 21:42:24 +0200

    Frontend app: set eslint ts

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,19 +3,25 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:vue/vue3-essential', 'airbnb-base'],
+  extends: ['plugin:vue/vue3-essential', 'airbnb-base', '@vue/typescript/recommended'],
   overrides: [
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    parser: '@typescript-eslint/parser',
   },
-  plugins: ['vue'],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
     'linebreak-style': 'off',
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
     'vue/multi-word-component-names': 'off',
     'no-use-before-define': 'off',
+    'import/prefer-default-export': 'off',
+    camelcase: 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-undef': 'off',
   },
 };
