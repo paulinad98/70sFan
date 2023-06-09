fe05f5e96720bfe0f7a9e7b8df13de443454202e
Paulina <paulinaklempka@gmail.com>
2023-06-09 19:31:48 +0200

    API: set new origin url

--- api/index.js
+++ api/index.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
     "http://127.0.0.1:5173",
     "https://70s-fan-front.vercel.app",
     "http://localhost:3000",
+    "https://70s-fan-5ahe.vercel.app/"
   ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
